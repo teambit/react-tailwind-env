8ad2eb6a7c54eb9037c08537fdd210ea4c440966
Gilad Shoham <shoham.gilad@gmail.com>
2021-06-20 14:20:40 +0300

    changes required to support new webpack config structure - see here - https://github.com/teambit/bit/pull/4433

--- components/envs/tailwind-bit/envs/tailwind-react/webpack/transformers.ts
+++ components/envs/tailwind-bit/envs/tailwind-react/webpack/transformers.ts
@@ -1,11 +1,10 @@
 import { WebpackConfigTransformer, WebpackConfigMutator } from "@teambit/webpack";
-import { cssNoModulesRegex } from "@teambit/modules.style-regexps";
+import * as stylesRegexps from "@teambit/webpack.modules.style-regexps";
 import tailwindcssPlugin from "tailwindcss";
+
 import autoprefixerPlugin from "autoprefixer";
 
-export const previewConfig: WebpackConfigTransformer = (
-  config: WebpackConfigMutator
-) => {
+function addTailwindConfig(config: WebpackConfigMutator): WebpackConfigMutator {
   // @ts-ignore
   // console.log(config.raw.module.rules);
   const oneOfRule = findOneOfRuleInPreviewConfig(config.raw.module.rules);
@@ -15,37 +14,40 @@ export const previewConfig: WebpackConfigTransformer = (
       "css rule not found. this probably means the webpack config of bit itself has changed"
     );
   }
-  // In the preview config we already has a postcss loader
+  // we already has a postcss loader
   const postcssLoader = findPostcssLoaderInRule(cssRule.use);
   if (!postcssLoader) {
     throw new Error(
       "postcss loader not found. this probably means the webpack config of bit itself has changed"
     );
   }
-  postcssLoader.options.postcssOptions.plugins.push(tailwindcssPlugin);
-  const tailwindPostcss = getTailwindPostcss();
-  if (cssRule?.use) {
-    cssRule.use.push(tailwindPostcss);
-  }
+  postcssLoader.options.postcssOptions.plugins.unshift(tailwindcssPlugin);
+  // const tailwindPostcss = getTailwindPostcss();
+  // if (cssRule?.use) {
+  //   cssRule.use.push(tailwindPostcss);
+  // }
   return config;
+}
+
+export const previewConfig: WebpackConfigTransformer = (
+  config: WebpackConfigMutator
+) => {
+  return addTailwindConfig(config);
 };
 export const devServerConfig: WebpackConfigTransformer = (
   config: WebpackConfigMutator
 ) => {
-  const cssRule = findCssRuleByCssNoModuleRegexp(config?.raw?.module?.rules);
-  if (!cssRule) {
-    throw new Error('css rule not found. this probably means the webpack config of bit itself has changed')
-  }
-  const tailwindPostcss = getTailwindPostcss();
-  if (cssRule?.use) {
-    cssRule.use.push(tailwindPostcss);
-  }
-  return config;
+  console.log(config.raw.module.rules[1].oneOf[0].use[2].options.postcssOptions.plugins)
+  const newConfig = addTailwindConfig(config);
+  console.log(newConfig.raw.module.rules[1].oneOf[0].use[2].options.postcssOptions.plugins[0].toString())
+  // console.log(require("util").inspect(newConfig.raw, { depth : 12}));
+  // throw new Error('gilad')
+  return newConfig;
 };
 
 function findCssRuleByCssNoModuleRegexp(rules: Array<any> = []) {
   return rules.find(
-    (rule) => rule.test.toString() === cssNoModulesRegex.toString()
+    (rule) => rule.test.toString() === stylesRegexps.cssNoModulesRegex.toString()
   );
 }
 
